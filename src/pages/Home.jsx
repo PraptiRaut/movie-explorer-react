a57dc47bfa317e6663d576940f8209bdb8b2108b
Prapti Raut <praptiraut03@gmail.com>
2026-05-14 17:17:51 +0530

    Fetch and display popular movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import { getPopularMovies } from "../services/movieApi";
+
 function Home() {
-    return <h1>Home Page</h1>
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function loadMovies() {
+            const popularMovies = await getPopularMovies();
+
+            setMovies(popularMovies);
+
+        }
+        loadMovies();
+    }, []);
+    return (
+        <div>
+            <h1>Popular Movies</h1>
+            {movies.map((movie) => (
+                <p key={movie.id}>{movie.title}</p>
+            ))}
+        </div>
+    );
 }
 
 export default Home;
