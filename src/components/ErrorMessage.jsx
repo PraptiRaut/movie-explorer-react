3343ff9afd4fa2b9ac4960c5ec6be18894e8feb7
Prapti Raut <praptiraut03@gmail.com>
2026-05-16 13:56:27 +0530

    Add loading and error handling states

---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,9 @@
+function ErrorMessage({ message }) {
+    return (
+        <div className="error-message">
+            <h2>{message}</h2>
+        </div>
+    );
+}
+
+export default ErrorMessage;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,33 +2,59 @@ import { useEffect, useState } from "react";
 import { getPopularMovies, searchMovies } from "../services/movieApi";
 import MovieCard from "../components/MovieCard";
 import SearchBar from "../components/SearchBar";
+import ErrorMessage from "../components/ErrorMessage";
+import Loader from "../components/Loader";
 
 
 function Home({ favorites, addToFavorites }) {
 
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [loading, setLoading] = useState("");
+    const [error, setError] = useState("");
 
     useEffect(() => {
         async function loadMovies() {
-            const popularMovies = await getPopularMovies();
+            try {
+                setLoading(true);
+                setError("");
 
-            setMovies(popularMovies);
+                const popularMovies = await getPopularMovies();
 
+                setMovies(popularMovies);
+            } catch (error) {
+                setError("Failed to load movies");
+            } finally {
+                setLoading(false);
+            }
         }
         loadMovies();
     }, []);
 
     async function handleSearch() {
-        if (!searchTerm.trim()) {
-            const popularMovies = await getPopularMovies();
-            setMovies(popularMovies);
-            return;
+        try {
+            setLoading(true);
+            setError("");
+            if (!searchTerm.trim()) {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+                return;
+            }
+            const results = await searchMovies(searchTerm);
+            setMovies(results);
+
+        } catch (error) {
+            setError("Failed to search movies.");
+        } finally {
+            setLoading(false);
         }
+    }
 
-        const results = await searchMovies(searchTerm);
-        setMovies(results);
-
+    if (loading) {
+        return <Loader />;
+    }
+    if (error) {
+        return <ErrorMessage message={error} />
     }
     return (
         <div>
