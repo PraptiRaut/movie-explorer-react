92dca3aa7d0d87ab4c24e40b129687500daf6c3e
Prapti Raut <praptiraut03@gmail.com>
2026-05-18 20:36:58 +0530

    Optimize MovieCard rendering with React.memo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import React from "react";
 
 function MovieCard({ movie, favorites, addToFavorites, removeFromFavorites }) {
 
@@ -40,4 +41,4 @@ function MovieCard({ movie, favorites, addToFavorites, removeFromFavorites }) {
     );
 }
 
-export default MovieCard;
+export default React.memo(MovieCard);
